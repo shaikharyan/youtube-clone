bfabea58ad05aa227d5365d9f42f59fe133a199a
Aryan Shaikh <arsahiyansk2000@gmail.com>
2023-07-13 12:23:06 +0530

    Add files via upload

---
+++ src/utils/api.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube138.p.rapidapi.com";
+
+const options = {
+    params: { hl: "en", gl: "US" },
+    headers: {
+        'X-RapidAPI-Key': 'e5960360ddmsh203779017c2174ep145350jsnc1a24fcbe168',
+        "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+    },
+};
+
+export const fetchDataFromApi = async (url) => {
+    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+    return data;
+};
+
+  
